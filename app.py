bf4b82304e5a997714b53dc631ceb977e02ea6ff
Hamza <hamza@heo-systems.com>
2024-03-17 06:09:53 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import typer
+from rich import print
+
+# im bored i will look into this later
+
+def main():
+
+    choice = typer.prompt("Your choice")
+
+if __name__ == '__main__':
+    typer.run(main)
